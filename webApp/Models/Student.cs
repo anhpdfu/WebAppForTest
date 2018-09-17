ee46a89a6a2cc9527e09e436cb34895ef80789f9
anhpdfu <anhpdfu@gmail.com>
2018-09-17 11:31:45 +0700

    Change model

--- webApp/Models/Student.cs
+++ webApp/Models/Student.cs
@@ -1,9 +1,13 @@
-﻿namespace webApp.Models
+﻿using System;
+
+namespace webApp.Models
 {
     public class Student
     {
         public int StudentId { get; set; }
         public int RoleNumber { get; set; }
         public string Name { get; set; }
+        public bool Active { get; set; }
+        public DateTime Dob { get; set; }
     }
 }
